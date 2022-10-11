edb1fe30053dabfe42ec3e9660a242ed7f0e2739
Victor Ruiz Amaro <101755374+vicszeddred@users.noreply.github.com>
2022-10-10 21:41:06 -0500

    Turn three cards reading into a function

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ def card_picker():
     card_picked = majorCards.pop(random.randint(0, len(majorCards) - 1))
     return card_picked
 
-print("TAROT READING")
-enter = input("Press ENTER to shuffle the cards")
-random.shuffle(majorCards)
-enter = input("Now press ENTER to pick the first card")
-card1 = card_picker()
-enter = input("Now press ENTER to pick the second card")
-card2 = card_picker()
-enter = input("Now press ENTER to pick the third card")
-card3 = card_picker()
+def threeCards():
+    print("TAROT READING")
+    enter = input("Press ENTER to shuffle the cards")
+    random.shuffle(majorCards)
+    enter = input("Now press ENTER to pick the first card")
+    past = card_picker()
+    enter = input("Now press ENTER to pick the second card")
+    present = card_picker()
+    enter = input("Now press ENTER to pick the third card")
+    future = card_picker()
 
-#print(majorCards)
-print(card1)
-print(card2)
-print(card3)
+
+    print(past)
+    print(present)
+    print(future)
